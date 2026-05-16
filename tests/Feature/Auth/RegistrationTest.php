dc5a1625007b252d5adf83fae0b5f8212a871f19
kortby@gmail.com <kortby@gmail.com>
2026-05-16 12:44:56 -0700

    test: add language field to registration and profile update feature tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -18,6 +18,7 @@ test('new users can register', function () {
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
+        'language' => 'en',
     ]);
 
     $this->assertAuthenticated();
